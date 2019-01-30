db87507105e171f2d66627303ce7ab2962157ece
emmayyg <marshall.ma@ericsson.com>
2019-01-30 21:08:30 +0800

    0.4.0

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-
+export { resolveJSONSchema, generateJoi, formatJoi, Options, SubSchemas } from './joi';
--- src/json-diff.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'json-diff' {
-  export interface Options {
-    keysOnly: boolean,
-  }
-
-  export function diff(obj1: any, obj2: any, options: Partial<Options> = {}): any;
-}
