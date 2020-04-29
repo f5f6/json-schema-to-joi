de12b48cfe6c34f6804b538d761a111043c773b8
ufonion <mayongcong@gmail.com>
2020-04-29 14:44:03 +0800

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export { resolveJSONSchema, generateJoiStatement, formatJoi, Options, SubSchemas } from './joi';
+export { 
+  resolveJSONSchema, resolveBundledJSONSchema, 
+  generateJoiStatement, JoiStatement,
+  formatJoi, Options, SubSchemas, 
+} from './joi';
 import * as Joi from './extendedJoi';
 export { Joi };
 export { AllOfSchema } from './extendedJoi/allOf';
