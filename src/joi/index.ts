d2d81adf3d77806333f3d64ebce8cbd052db0b90
ufonion <mayongcong@gmail.com>
2020-04-29 14:08:39 +0800

    export function resolveBundledJSONSchema

--- src/joi/index.ts
+++ src/joi/index.ts
@@ -1,4 +1,4 @@
-export { resolveJSONSchema } from './resolve';
+export { resolveJSONSchema, resolveBundledJSONSchema } from './resolve';
 export { ResolveOptions as Options, SubSchemas } from './options';
 export { generateJoiStatement } from './generate';
 export { formatJoi } from './format';
