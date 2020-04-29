6b665bf4d1f42a56955adfcef7562126a5c37fc7
Marshall Ma <marshall.ma@ericsson.com>
2020-04-29 14:44:03 +0800

    fix export

--- src/joi/index.ts
+++ src/joi/index.ts
@@ -1,4 +1,4 @@
 export { resolveJSONSchema, resolveBundledJSONSchema } from './resolve';
 export { ResolveOptions as Options, SubSchemas } from './options';
-export { generateJoiStatement } from './generate';
+export { generateJoiStatement, JoiStatement } from './generate';
 export { formatJoi } from './format';
